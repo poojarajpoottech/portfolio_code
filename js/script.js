4b2f6acfe4a84f02a64beca950b013a8dadb8ada
Pooja Rajpoot <131517875+poojarajpoottech@users.noreply.github.com>
2023-04-27 16:15:22 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// typing functionlity
+var typed = new Typed(".typing", {
+  strings: [
+    "UI/UX Designer",
+    "Graphics Designer",
+    "Web Designer",
+    "JavaScript",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
